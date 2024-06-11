4667b716c66ce0fac017c5d46154f9c949e04017
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-06-11 16:10:53 +0200

    api url

--- web/src/App.js
+++ web/src/App.js
@@ -3,9 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [greetings, setGreetings]  = useState('world')
+  const [greetings, setGreetings] = useState('world')
+
+  const API_URL = process.env.REACT_APP_API_URL
 
   // Vous devez faire ici un call http auprès de l'API.
+  useEffect(() => {
+    function getGrretings() {
+      return fetch(`${API_URL}/greetings`)
+      .then(response => response.json())
+      .then(data => {
+        setGreetings(data.greetings)
+      })
+      .catch(err => console.error(err))
+    }
+    getGrretings()
+  }, [])
+
 
   return (
     <div className="App">
